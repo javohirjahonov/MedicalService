f5b51e38806d7e67453d302480ed2480bc4d094a
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-24 21:54:32 +0500

    Added Education, ScientificAchievements, WorkExperience for when create doctor add information and any configuration

---
+++ MedicalService/src/main/java/com/example/medicalservice/entity/doctorInformations/education/Education.java
@@ -0,0 +1,39 @@
+package com.example.medicalservice.entity.doctorInformations.education;
+
+import com.example.medicalservice.entity.BaseEntity;
+import com.example.medicalservice.entity.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Duration;
+import java.util.List;
+
+@Entity(name = "educations")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Education extends BaseEntity {
+
+    @Column(nullable = false)
+    private String educationType;
+
+    @Column(nullable = false)
+    private String education_institution;
+
+    @Column(nullable = false)
+    private String faculty;
+
+    @Column(nullable = false)
+    private String additionally;
+
+    @Column(nullable = false)
+    private String specialization;
+
+    @Column(nullable = false)
+    private Duration periodOfStudy;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<UserEntity> userEducations;
+}
---
+++ MedicalService/src/main/java/com/example/medicalservice/entity/doctorInformations/scientificAchievements/ScientificAchievements.java
@@ -0,0 +1,32 @@
+package com.example.medicalservice.entity.doctorInformations.scientificAchievements;
+
+import com.example.medicalservice.entity.BaseEntity;
+import com.example.medicalservice.entity.user.UserEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.List;
+
+@Entity(name = "scientific_achievements")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ScientificAchievements extends BaseEntity {
+
+    @Column(nullable = false)
+    private String qualification;
+
+    @Column(nullable = false)
+    private String academicDegree;
+
+    @Column(nullable = false)
+    private String ranks;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<UserEntity> userScientificAchievements;
+}
