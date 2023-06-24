6dbc59264be66e0d8377a2f64e472a1aec1bbbcf
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-24 19:55:53 +0500

    Added UserController and configuration

--- MedicalService/src/main/java/com/example/medicalservice/controller/UserController.java
+++ MedicalService/src/main/java/com/example/medicalservice/controller/UserController.java
@@ -1,11 +1,32 @@
 package com.example.medicalservice.controller;
 
+import com.example.medicalservice.dto.request.UserRequestDto;
+import com.example.medicalservice.entity.user.UserEntity;
+import com.example.medicalservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    @PutMapping("/update")
+    public ResponseEntity<UserEntity> update(
+            @RequestBody UserRequestDto userRequestDto,
+            Principal principal
+    ){
+        return ResponseEntity.ok(userService.update(userRequestDto,principal));
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(
+            Principal principal
+    ){
+        userService.delete(principal);
+        return ResponseEntity.ok("Successfully deleted");
+    }
 
 }
