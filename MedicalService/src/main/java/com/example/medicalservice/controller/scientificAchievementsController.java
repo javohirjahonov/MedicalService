49bb0948f56d4798055121fb32947e4c3c8eade5
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-25 15:06:18 +0500

    Added scientificAchievementsController

---
+++ MedicalService/src/main/java/com/example/medicalservice/controller/scientificAchievementsController.java
@@ -0,0 +1,47 @@
+package com.example.medicalservice.controller;
+
+import com.example.medicalservice.dto.education.EducationCreateDto;
+import com.example.medicalservice.dto.scientificAchievements.ScientificAchievementsCreateDto;
+import com.example.medicalservice.entity.doctorInformations.education.EducationEntity;
+import com.example.medicalservice.entity.doctorInformations.scientificAchievements.ScientificAchievementsEntity;
+import com.example.medicalservice.service.education.EducationService;
+import com.example.medicalservice.service.scientificAchievements.ScientificAchievementsService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/api/v1/scientificAchievements")
+@RequiredArgsConstructor
+public class scientificAchievementsController {
+
+    private final ScientificAchievementsService scientificAchievementsService;
+    private final ModelMapper modelMapper;
+    @PostMapping("/add")
+    public ResponseEntity<ScientificAchievementsEntity> addScientificAchievements (
+            @RequestBody ScientificAchievementsCreateDto scientificAchievementsCreateDto
+    ){
+        return ResponseEntity.ok(scientificAchievementsService.add(scientificAchievementsCreateDto));
+    }
+    @PutMapping("/update")
+    public ResponseEntity<String> updateScientificAchievements (
+            @RequestBody ScientificAchievementsCreateDto scientificAchievementsCreateDto,
+            @RequestParam(value = "id") UUID id
+    ){
+        scientificAchievementsService.update(scientificAchievementsCreateDto, id);
+        return ResponseEntity.ok("Data successfully updated");
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(
+            @RequestParam(value = "id") UUID id
+    ){
+        scientificAchievementsService.delete(id);
+        return ResponseEntity.ok("Data successfully deleted");
+    }
+
+
+}
